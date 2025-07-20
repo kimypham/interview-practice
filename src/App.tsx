02b96161f80a24250cc8172a2e3aec7f71aa8d5e
Kim Pham <km.phm2@gmail.com>
2025-07-20 23:14:32 +1000

    Initial set up for question

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import DataTable from './components/DataTable';
 
 function App() {
     return (
-        <div className="App">
-            <p>Hello World! (And hello Canva!)</p>
+        <div>
+            <DataTable />
         </div>
     );
 }
